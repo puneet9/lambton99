35895d2caa2eda0ac8b4eea6551ab88d98de16d2
puneet9 <puneet9.bindra@gmail.com>
2014-05-06 14:27:17 -0400

    Initial Commit

---
+++ lambton99/lambton99/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  lambton99
+//
+//  Created by User1 on 2014-05-06.
+//  Copyright (c) 2014 puneet. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
